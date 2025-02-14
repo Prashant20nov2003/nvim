8376ce5aea4609f6a432f3ee6c00ab1781f08c88
Prashant20nov2003 <prashanttbhardwajj@gmail.com>
2025-02-14 03:13:10 +0000

    added more keymaps

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,32 +1,120 @@
 -- set leader key to space
 vim.g.mapleader = " "
-
 local keymap = vim.keymap -- for conciseness
 
 ---------------------
--- General Keymaps -------------------
+-- General Keymaps
+---------------------
 
--- use jk to exit insert mode
+-- use jk/kj to exit insert mode
 keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
+keymap.set("i", "kj", "<ESC>", { desc = "Exit insert mode with kj" })
 
 -- clear search highlights
 keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
 
 -- delete single character without copying into register
--- keymap.set("n", "x", '"_x')
+keymap.set("n", "x", '"_x', { desc = "Delete single character without copying" })
+
+-- save file
+keymap.set("n", "<C-s>", "<cmd> w <CR>", { desc = "Save file" })
+
+-- save file without formatting
+keymap.set("n", "<leader>sn", "<cmd>noautocmd w<CR>", { desc = "Save file without formatting" })
+
+-- quit file
+keymap.set("n", "<C-q>", "<cmd> q <CR>", { desc = "Quit file" })
 
 -- markdown preview
-keymap.set("n", "<leader>mp", ":MarkdownPreviewToggle<cr>")
+keymap.set("n", "<leader>mp", ":MarkdownPreviewToggle<cr>", { desc = "Toggle markdown preview" })
 
--- window management
-keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" })     -- split window vertically
-keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" })   -- split window horizontally
-keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" })      -- make split windows equal width & height
-keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
+---------------------
+-- Window Management
+---------------------
 
--- tab management
-keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" })                     -- open new tab
-keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" })              -- close current tab
-keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" })                     --  go to next tab
-keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" })                 --  go to previous tab
-keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
+-- window splits
+keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" })
+keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" })
+keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" })
+keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" })
+
+-- window navigation
+keymap.set("n", "<C-h>", ":wincmd h<CR>", { desc = "Navigate to left window" })
+keymap.set("n", "<C-j>", ":wincmd j<CR>", { desc = "Navigate to bottom window" })
+keymap.set("n", "<C-k>", ":wincmd k<CR>", { desc = "Navigate to top window" })
+keymap.set("n", "<C-l>", ":wincmd l<CR>", { desc = "Navigate to right window" })
+
+-- window resizing
+keymap.set("n", "<Up>", ":resize -2<CR>", { desc = "Decrease window height" })
+keymap.set("n", "<Down>", ":resize +2<CR>", { desc = "Increase window height" })
+keymap.set("n", "<Left>", ":vertical resize -2<CR>", { desc = "Decrease window width" })
+keymap.set("n", "<Right>", ":vertical resize +2<CR>", { desc = "Increase window width" })
+
+---------------------
+-- Tab Management
+---------------------
+
+keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" })
+keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" })
+keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" })
+keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" })
+keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" })
+
+---------------------
+-- Buffer Management
+---------------------
+
+keymap.set("n", "<Tab>", ":bnext<CR>", { desc = "Go to next buffer" })
+keymap.set("n", "<S-Tab>", ":bprevious<CR>", { desc = "Go to previous buffer" })
+keymap.set("n", "<leader>x", ":Bdelete!<CR>", { desc = "Close current buffer" })
+keymap.set("n", "<leader>b", "<cmd>enew<CR>", { desc = "Create new buffer" })
+
+---------------------
+-- Text Manipulation
+---------------------
+
+-- stay in indent mode
+keymap.set("v", "<", "<gv", { desc = "Decrease indent and stay in visual mode" })
+keymap.set("v", ">", ">gv", { desc = "Increase indent and stay in visual mode" })
+
+-- move text up and down
+keymap.set("v", "<A-j>", ":m .+1<CR>==", { desc = "Move text down" })
+keymap.set("v", "<A-k>", ":m .-2<CR>==", { desc = "Move text up" })
+
+-- keep last yanked when pasting
+keymap.set("v", "p", '"_dP', { desc = "Paste without overwriting register" })
+
+-- replace word under cursor
+keymap.set("n", "<leader>j", '*``cgn', { desc = "Replace word under cursor" })
+
+-- clipboard operations
+keymap.set({ "n", "v" }, "<leader>y", [["+y]], { desc = "Yank to system clipboard" })
+keymap.set("n", "<leader>Y", [["+Y]], { desc = "Yank line to system clipboard" })
+
+---------------------
+-- Search and Navigation
+---------------------
+
+-- vertical scroll and center
+keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Scroll down and center" })
+keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Scroll up and center" })
+
+-- find and center
+keymap.set("n", "n", "nzzzv", { desc = "Next search result and center" })
+keymap.set("n", "N", "Nzzzv", { desc = "Previous search result and center" })
+
+---------------------
+-- Diagnostics
+---------------------
+
+keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous diagnostic" })
+keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next diagnostic" })
+keymap.set("n", "<leader>d", vim.diagnostic.open_float, { desc = "Show diagnostic float" })
+keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostics list" })
+
+---------------------
+-- Session Management
+---------------------
+
+keymap.set("n", "<leader>ss", ":mksession! .session.vim<CR>", { desc = "Save session", silent = false })
+keymap.set("n", "<leader>sl", ":source .session.vim<CR>", { desc = "Load session", silent = false })
--- lua/core/options.lua
+++ lua/core/options.lua
@@ -18,21 +18,23 @@ opt.ignorecase = true -- ignore case when searching
 opt.smartcase = true -- if you include mixed case in your search, assumes you want case-sensitive
 
 -- cursor line
-opt.cursorline = true -- highlight the current cursor line
+opt.cursorline = false -- highlight the current cursor line
+
+-- mouse mode
+opt.mouse = 'a' --Enable Mouse Mode (default: '')
 
 -- appearance
-
 -- turn on termguicolors for nightfly colorscheme to work
 -- (have to use iterm2 or any other true color terminal)
 opt.termguicolors = true
-opt.background = "dark" -- colorschemes that can be light or dark will be made dark
-opt.signcolumn = "yes" -- show sign column so that text doesn't shift
+opt.background = 'dark' -- colorschemes that can be light or dark will be made dark
+opt.signcolumn = 'yes' -- show sign column so that text doesn't shift
 
 -- backspace
-opt.backspace = "indent,eol,start" -- allow backspace on indent, end of line or insert mode start position
+opt.backspace = 'indent,eol,start' -- allow backspace on indent, end of line or insert mode start position
 
 -- clipboard
-opt.clipboard:append("unnamedplus") -- use system clipboard as default register
+opt.clipboard:append 'unnamedplus' -- use system clipboard as default register
 
 -- split windows
 opt.splitright = true -- split vertical window to the right
@@ -40,3 +42,27 @@ opt.splitbelow = true -- split horizontal window to the bottom
 
 -- turn off swapfile
 opt.swapfile = false
+
+-- Additional settings from the first configuration
+opt.updatetime = 250 -- Decrease update time for faster response
+opt.timeoutlen = 300 -- Time to wait for mapped sequence completion (ms)
+opt.completeopt = 'menuone,noselect' -- Set completeopt to have a better completion experience
+opt.scrolloff = 4 -- minimal number of screen lines to keep above and below the cursor
+opt.sidescrolloff = 8 -- minimal number of screen columns either side of cursor if wrap is `false`
+opt.softtabstop = 2 -- Number of spaces that a tab counts for while performing editing operations (matching your tabstop)
+opt.pumheight = 10 -- pop up menu height
+opt.showtabline = 2 -- always show tabs
+opt.cmdheight = 1 -- more space in the neovim command line for displaying messages
+opt.conceallevel = 0 -- so that `` is visible in markdown files
+opt.fileencoding = 'utf-8' -- the encoding written to a file
+opt.undofile = true -- Save undo history
+opt.writebackup = false -- if a file is being edited by another program, it is not allowed to be edited
+opt.breakindent = true -- Enable break indent
+opt.linebreak = true -- companion to wrap, don't split words
+opt.showmode = false -- we don't need to see things like -- INSERT -- anymore
+
+-- Additional format options
+opt.formatoptions:remove { 'c', 'r', 'o' } -- don't insert the current comment leader automatically
+opt.iskeyword:append '-' -- hyphenated words recognized by searches
+opt.shortmess:append 'c' -- don't give completion menu messages
+opt.runtimepath:remove '/usr/share/vim/vimfiles' -- separate vim plugins from neovim
